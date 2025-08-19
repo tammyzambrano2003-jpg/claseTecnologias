23981836c51bc0c0cf800145b74901aaf38d08c4
tammyzambrano2003-jpg <tammyzambrano2003@gmail.com>
2025-08-19 01:07:57 -0600

    ejemplo clase (#1)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore,provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() => initializeApp({
+  apiKey: "AIzaSyCcxtP6_SB8SPi1Icb7fl9MHHSX_4j86_Y",
+  authDomain: "clasetecnologias-d8af3.firebaseapp.com",
+  projectId: "clasetecnologias-d8af3",
+  storageBucket: "clasetecnologias-d8af3.firebasestorage.app",
+  messagingSenderId: "451524088921",
+  appId: "1:451524088921:web:672f0acb6e3b076cd5cd48",
+  measurementId: "G-VJ3J8F1X72"
+})),
+provideFirestore(() => getFirestore()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
--- src/app/components/componente1/componente1.ts
+++ src/app/components/componente1/componente1.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Componente2 } from "../componente2/componente2";
+import { Persona } from '../../servicios/persona';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-componente1',
-  imports: [Componente2],
+  imports: [JsonPipe],
   templateUrl: './componente1.html',
   styleUrl: './componente1.scss'
 })
 export class Componente1 {
+  personaService = inject(Persona);
+personas = []
+constructor() {
+  this.getPersonas();
+}
+   agregarPersona(){
+    this.personaService.addPersona({
+      nombre: 'Nuevo Usuario',
+      edad: 30,
+      email: 'tammy.zambrano@unitec.edu',
+      telefono: '1234567890',
 
+    });
+  }
+  getPersonas(){
+    this.personaService.getPersonas().subscribe((data: any) => {
+      this.personas = data;
+    }) 
+  }
 }
