43c26d3fe20eb7199ae694c78efbe061e067ed65
tammyzambrano2003-jpg <tammyzambrano2003@gmail.com>
2025-08-19 15:42:40 -0600

    fin de clase

--- src/app/servicios/persona.ts
+++ src/app/servicios/persona.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +17,18 @@ addPersona(persona:any){
  const personaCollection = collection(this.firestore, 'personas');
  return addDoc(personaCollection, persona); 
 }
-updatePersona(persona:any){}
+updatePersona(persona:any){
+  const personaCollection = collection(this.firestore, 'personas');
+  const personaDoc = doc(personaCollection, persona.uuid);
+  return updateDoc(personaDoc, persona);
+}
 
-deletePersona(personaid:number){}
+deletePersona(personaId:string){
+  const personaCollection = collection(this.firestore, 'personas');
+  const personaDoc = doc(personaCollection, personaId);
+  return deleteDoc(personaDoc);
+
+}
+
 
 }
